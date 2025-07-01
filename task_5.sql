809db7499e7fc0dd20c8ca717aab4e793ca6e6e9
keroojoseph <keroojoseph0@gmail.com>
2025-07-02 00:18:31 +0300

    MODIFY task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,7 +1,7 @@
 USE alx_book_store;
 
 INSERT INTO 
-	Customers (customer_name, email, address) 
+	Customers (customer_id, customer_name, email, address) 
 VALUES
-	("Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.");
+	(1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.");
     
